49aab95068ab359a5b66055a31ea94d00850d62c
zariganitosh <XXXX@example.com>
2014-09-21 15:12:23 +0900

    不要になったJPCalendarモジュールを削除

--- jcal.rb
+++ jcal.rb
@@ -1,103 +1,101 @@
 #!/usr/bin/ruby -W0
 # encoding: utf-8
 
-module JPCalendar
-  require 'date'
+require 'date'
 
-  class JPDate < Date
-    HOLIDAYS = [
-      {month:4,  day:10,          term:1959..1959, name:'結婚の儀'},
-      {month:2,  day:24,          term:1989..1989, name:'大喪の礼'},
-      {month:11, day:12,          term:1990..1990, name:'即位の礼'},
-      {month:6,  day:9,           term:1993..1993, name:'結婚の儀'},
-      {month:1,  day:1,           term:   0..9999, name:'元旦'},
-      {month:1,  day:15,          term:   0..1999, name:'成人の日'},
-      {month:1,  day:'monday 2',  term:2000..9999, name:'成人の日'},
-      {month:2,  day:11,          term:1967..9999, name:'建国記念日'},
-      {month:3,  day:'spring_day',term:1900..2099, name:'春分の日'},
-      {month:4,  day:29,          term:   0..1988, name:'天皇誕生日'},
-      {month:4,  day:29,          term:1989..2006, name:'みどりの日'},
-      {month:4,  day:29,          term:2007..9999, name:'昭和の日'},
-      {month:5,  day:3 ,          term:   0..9999, name:'憲法記念日'},
-      {month:5,  day:4 ,          term:2007..9999, name:'みどりの日'},
-      {month:5,  day:5 ,          term:   0..9999, name:'こどもの日'},
-      {month:7,  day:20,          term:1996..2002, name:'海の日'},
-      {month:7,  day:'monday 3',  term:2003..9999, name:'海の日'},
-      {month:8,  day:11,          term:2016..9999, name:'山の日'},
-      {month:9,  day:15,          term:1966..2002, name:'敬老の日'},
-      {month:9,  day:'monday 3',  term:2003..9999, name:'敬老の日'},
-      {month:9,  day:'autumn_day',term:1900..2099, name:'秋分の日'},
-      {month:10, day:10,          term:1966..1999, name:'体育の日'},
-      {month:10, day:'monday 2',  term:2000..9999, name:'体育の日'},
-      {month:11, day:3,           term:   0..9999, name:'文化の日'},
-      {month:11, day:23,          term:   0..9999, name:'勤労感謝の日'},
-      {month:12, day:23,          term:1989..9999, name:'天皇誕生日'},
-    ]
-    @@holidays_database = nil
+class JPDate < Date
+  HOLIDAYS = [
+    {month:4,  day:10,          term:1959..1959, name:'結婚の儀'},
+    {month:2,  day:24,          term:1989..1989, name:'大喪の礼'},
+    {month:11, day:12,          term:1990..1990, name:'即位の礼'},
+    {month:6,  day:9,           term:1993..1993, name:'結婚の儀'},
+    {month:1,  day:1,           term:   0..9999, name:'元旦'},
+    {month:1,  day:15,          term:   0..1999, name:'成人の日'},
+    {month:1,  day:'monday 2',  term:2000..9999, name:'成人の日'},
+    {month:2,  day:11,          term:1967..9999, name:'建国記念日'},
+    {month:3,  day:'spring_day',term:1900..2099, name:'春分の日'},
+    {month:4,  day:29,          term:   0..1988, name:'天皇誕生日'},
+    {month:4,  day:29,          term:1989..2006, name:'みどりの日'},
+    {month:4,  day:29,          term:2007..9999, name:'昭和の日'},
+    {month:5,  day:3 ,          term:   0..9999, name:'憲法記念日'},
+    {month:5,  day:4 ,          term:2007..9999, name:'みどりの日'},
+    {month:5,  day:5 ,          term:   0..9999, name:'こどもの日'},
+    {month:7,  day:20,          term:1996..2002, name:'海の日'},
+    {month:7,  day:'monday 3',  term:2003..9999, name:'海の日'},
+    {month:8,  day:11,          term:2016..9999, name:'山の日'},
+    {month:9,  day:15,          term:1966..2002, name:'敬老の日'},
+    {month:9,  day:'monday 3',  term:2003..9999, name:'敬老の日'},
+    {month:9,  day:'autumn_day',term:1900..2099, name:'秋分の日'},
+    {month:10, day:10,          term:1966..1999, name:'体育の日'},
+    {month:10, day:'monday 2',  term:2000..9999, name:'体育の日'},
+    {month:11, day:3,           term:   0..9999, name:'文化の日'},
+    {month:11, day:23,          term:   0..9999, name:'勤労感謝の日'},
+    {month:12, day:23,          term:1989..9999, name:'天皇誕生日'},
+  ]
+  @@holidays_database = nil
 
-    def holiday
-      build_holiday(year) if year != holiday_year
-      @@holidays_database.assoc(self).to_a.last
+  def holiday
+    build_holiday(year) if year != holiday_year
+    @@holidays_database.assoc(self).to_a.last
+  end
+
+  def holiday_year
+    @@holidays_database ? @@holidays_database[0][0].year : nil
+  end
+
+  def build_holiday(y)
+    # 有効な祝日を取り出し、日付を追加する
+    enable_holidays = HOLIDAYS.select {|h| h[:term].include?(y)}.map do |h|
+      case h[:day]
+      when Fixnum
+        {date: Date.new(y, h[:month], h[:day])}.merge(h)
+      when String
+        {date: send(*h[:day].split, y, h[:month])}.merge(h)
+      end
     end
 
-    def holiday_year
-      @@holidays_database ? @@holidays_database[0][0].year : nil
-    end
+    enable_dates = enable_holidays.map {|h| h[:date]}
 
-    def build_holiday(y)
-      # 有効な祝日を取り出し、日付を追加する
-      enable_holidays = HOLIDAYS.select {|h| h[:term].include?(y)}.map do |h|
-        case h[:day]
-        when Fixnum
-          {date: Date.new(y, h[:month], h[:day])}.merge(h)
-        when String
-          {date: send(*h[:day].split, y, h[:month])}.merge(h)
+    # 振替休日を判定
+    enable_dates.each do |date|
+      if date.wday == 0
+        while enable_dates.include?(date)
+          date += 1
         end
-      end
-
-      enable_dates = enable_holidays.map {|h| h[:date]}
-
-      # 振替休日を判定
-      enable_dates.each do |date|
-        if date.wday == 0
-          while enable_dates.include?(date)
-            date += 1
-          end
-          enable_holidays << {date:date, name:'振替休日'}
-        end
-      end
-
-      # 国民の休日を判定
-      enable_dates.each_cons(2) do |a, b|
-        if b.day - a.day == 2 && (a + 1).wday != 0 && !enable_holidays.map {|h| h[:date]}.include?(a + 1)
-          enable_holidays << {date:a + 1, name:'国民の休日'}
-        end
-      end
-
-      @@holidays_database = enable_holidays.map {|h| [h[:date], h[:name]]}.sort
-    end
-
-    def monday(w, y, m)
-      Date.new(y, m, 7 * w.to_i - ((Date.new(y, m) - 1).wday + 6) % 7)
-    end
-
-    def spring_day(y, m)
-      case y
-      when 1900..2099
-        dy = y - 1900
-        Date.new(y, m, (21.4471 + 0.242377*dy - dy/4).to_i)
+        enable_holidays << {date:date, name:'振替休日'}
       end
     end
 
-    def autumn_day(y, m)
-      case y
-      when 1900..2099
-        dy = y - 1900
-        Date.new(y, m, (23.8896 + 0.242032*dy - dy/4).to_i)
+    # 国民の休日を判定
+    enable_dates.each_cons(2) do |a, b|
+      if b.day - a.day == 2 && (a + 1).wday != 0 && !enable_holidays.map {|h| h[:date]}.include?(a + 1)
+        enable_holidays << {date:a + 1, name:'国民の休日'}
       end
     end
-  end # class JPDate
-end # module JPCalendar
+
+    @@holidays_database = enable_holidays.map {|h| [h[:date], h[:name]]}.sort
+  end
+
+  def monday(w, y, m)
+    Date.new(y, m, 7 * w.to_i - ((Date.new(y, m) - 1).wday + 6) % 7)
+  end
+
+  def spring_day(y, m)
+    case y
+    when 1900..2099
+      dy = y - 1900
+      Date.new(y, m, (21.4471 + 0.242377*dy - dy/4).to_i)
+    end
+  end
+
+  def autumn_day(y, m)
+    case y
+    when 1900..2099
+      dy = y - 1900
+      Date.new(y, m, (23.8896 + 0.242032*dy - dy/4).to_i)
+    end
+  end
+end # class JPDate
 
 module JcalEx
   refine String do
@@ -126,7 +124,6 @@ end
 using JcalEx
 
 module Jcal
-  include JPCalendar
   WEEK_JA = %w(日 月 火 水 木 金 土)
 
   module_function
